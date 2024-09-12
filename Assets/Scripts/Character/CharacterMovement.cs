1ab2dbc3ce0e8431d2fe47ca6af49123ca67259b
unknown <sarban@mail.ru>
2024-09-12 21:00:42 +0300

    second commit

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class CharacterMovement : MonoBehaviour
 {
@@ -16,9 +18,22 @@ public class CharacterMovement : MonoBehaviour
     float moveX;
     float moveY;
 
+    float moveXidle;
+    float moveYidle;
+
+    Animator animator;
+    EventSystem IPointerDownHandler = null;
+
     void Start()
     {
+        if (IPointerDownHandler != null)
+        {
+            Debug.Log("ASDSADSAD");
+        }
+
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
@@ -28,10 +43,39 @@ public class CharacterMovement : MonoBehaviour
         moveY = Input.GetAxis("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+
+        if (moveX != 0 || moveY != 0)
+        {
+            Animate(moveX, moveY, 1);
+            moveXidle = moveX;
+            moveYidle = moveY;
+        }
+        else
+        {
+            Animate(moveXidle, moveYidle, 0);
+        }
     }
 
     private void FixedUpdate()
     {
         controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    void Animate(float moveX, float moveY, int layer)
+    {
+        switch (layer)
+        {
+            case 0:
+                animator.SetLayerWeight(0, 1);
+                animator.SetLayerWeight(1, 0);
+                break;
+            case 1:
+                animator.SetLayerWeight(1, 1);
+                animator.SetLayerWeight(0, 0);
+                break;
+        }
+
+        animator.SetFloat("MoveX", moveX);
+        animator.SetFloat("MoveY", moveY);
     }
 }
